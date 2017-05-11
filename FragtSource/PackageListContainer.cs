cbb477a42c8ae340385d05fa7fba2a66b4956cdf
Shievo <Shievo@DESKTOP-C65P8PN>
2017-05-11 15:11:34 +0200

    Tilføjede Package Object

---
+++ FragtSource/PackageListContainer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FragtSource
+{
+    class PackageListContainer
+    {
+        List<Package> packageList = new List<Package>();
+
+        public PackageListContainer()
+        {
+            CreateExamplePackages();
+        }
+
+        private void CreateExamplePackages()
+        {
+            Package package1 = new Package();
+            package1.dato = "01/01/2017";
+        }
+    }
+}
