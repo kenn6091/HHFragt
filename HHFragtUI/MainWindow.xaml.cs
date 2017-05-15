134b81b9df92bf6220775b7ebd45e0c93928da39
Mithrall <kenneth-dorn@hotmail.com>
2017-05-15 18:15:27 +0200

    Total items livesynch

--- HHFragtUI/MainWindow.xaml.cs
+++ HHFragtUI/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,10 +19,10 @@ namespace HHFragtUI {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow:Window
-    {
+    public partial class MainWindow:Window {
         public List<Package> packageList = new List<Package>();
         public PackageListController PLC = new PackageListController();
+
         public MainWindow()
         {
             packageList = FetchPackageListFromController();
@@ -34,15 +35,13 @@ namespace HHFragtUI {
         private void Btn_gem(object sender, RoutedEventArgs e)
         {
             Package tempPackage = new Package();
-            tempPackage.Date = UdkastDato.Text;
+            tempPackage.Date = UdkastDate.Text;
             tempPackage.Type = UdkastType.Text;
             tempPackage.Country = UdkastCountry.Text;
             tempPackage.Price = UdkastPrice.Text;
             tempPackage.Comment = UdkastComment.Text;
             
-            PLC.packageList.Add(tempPackage);
-            packageList = FetchPackageListFromController();
-            packageDatagrid.ItemsSource = packageList;
+            PLC.AddPackageToList(tempPackage);
             packageDatagrid.Items.Refresh();
         }
 
