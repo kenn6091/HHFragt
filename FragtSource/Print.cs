54bbd820dc2de8891e35f7ab7e6ccfff3d1d2383
Mithrall <kenneth-dorn@hotmail.com>
2017-05-17 15:09:10 +0200

    Save to file ! :D

---
+++ FragtSource/Print.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FragtSource {
+    public class Print {
+        public void Printall(List<Package> packages) {
+
+            string path = @Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\Print.csv";
+            TryToDelete(path);
+
+            string appendText1 = "Dato;Type;Pris;Land;Kommentar" + Environment.NewLine;
+            File.AppendAllText(path, appendText1, Encoding.UTF8);
+
+            foreach (Package package in packages) {
+                string appendText = package.Date.ToString("dd/MM/yyyy") + ";" + package.Type.ToString() + ";" + package.Price.ToString() + ";" + package.Country.ToString() + ";" + package.Comment + Environment.NewLine;
+                File.AppendAllText(path, appendText, Encoding.UTF8);
+            }
+        }
+
+        private void TryToDelete(string path) {
+            try {
+                File.Delete(path);
+            } catch {
+
+            }
+        }
+    }
+}
