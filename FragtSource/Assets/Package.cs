c32ce544491e8e369e1f3b524c60073ec634b36e
Mithrall <kenneth-dorn@hotmail.com>
2017-05-18 19:58:30 +0200

    Ryttet op i "using" Tilføjet en masse mht. database connection Virker stadig ik helt.

--- FragtSource/Assets/Package.cs
+++ FragtSource/Assets/Package.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace FragtSource
-{
+namespace FragtSource {
     public class Package
     {
         private DateTime _date;
